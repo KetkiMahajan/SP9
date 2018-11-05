4409dde483fe2e9a9fb1c9f5c6ade91699a1fa49
Ketki Rajendra Mahajan <31837601+KetkiMahajan@users.noreply.github.com>
2018-11-04 22:44:10 -0600

    Changes for threshold and take 3

--- src/SP9.java
+++ src/SP9.java
@@ -3,15 +3,16 @@
  *  Ketki Mahajan
  *  Anirudh Erabelly
  */
-
-import java.util.Arrays;
 import java.util.Random;
 
 public class SP9 {
     public static Random random = new Random();
     public static int numTrials = 100;
+    //We have chosen threshold to be 99 for better performance
+    public static int Threshold=99;
+
     public static void main(String[] args) {
-        int n = 49;
+        int n = 10000;
         int choice = 1 + random.nextInt(4);
         if(args.length > 0) {
             n = Integer.parseInt(args[0]);
@@ -29,27 +30,23 @@ public class SP9 {
                 Shuffle.shuffle(arr);
                 numTrials = 1;
                 insertionSort(arr);
-                System.out.println(Arrays.toString(arr));
                 break;
             case 2:
                 for(int i=0; i<numTrials; i++) {
                     Shuffle.shuffle(arr);
                     mergeSort1(arr);
-                    System.out.println(Arrays.toString(arr));
                 }
-                break;  // etc
+                break;
             case 3:
                 for(int i=0; i<numTrials; i++) {
                     Shuffle.shuffle(arr);
                     mergeSort2(arr);
-                    System.out.println(Arrays.toString(arr));
                 }
                 break;
             case 4:
                 for(int i=0; i<numTrials; i++) {
                     Shuffle.shuffle(arr);
                     mergeSort3(arr);
-                    System.out.println(Arrays.toString(arr));
                 }
                 break;
         }
@@ -59,32 +56,32 @@ public class SP9 {
         System.out.println("Choice: " + choice + "\n" + timer);
     }
 
-    //Insertionsort Implementation
+    //Insertion sort Implementation
     public static void insertionSort(int[] arr) {
         insertionSort(arr, 0, arr.length-1);
     }
 
     private static void insertionSort(int[] arr, int start, int end) {
-    	for(int i=1; i <= end; i++)
+        for(int i=start+1; i <= end; i++)
         {
             int temp= arr[i];
             int j=i-1;
-            while(j>=0 && temp<=arr[j]){
+            while(j>=start && temp<arr[j]){
                 arr[j+1]=arr[j];
                 j--;
             }
             arr[j+1] = temp;
         }
     }
-
+    //Merge sort Implementation
     public static void mergeSort1(int[] arr) {
         mergeSort(arr,0,arr.length-1);
     }
 
     private static void mergeSort(int[] arr, int p, int r) {
-        int q=0;
+
         if(p<r){
-            q=(p+r)/2;
+           int q=(p+r)/2;
             mergeSort(arr,p,q);
             mergeSort(arr,q+1,r);
             merge(arr,p,q,r);
@@ -110,21 +107,22 @@ public class SP9 {
     //mergesort(Take 2) Implementation
     public static void mergeSort2(int[] arr) {
     	int[] arrCopy = new int[arr.length];
-    	mergeSort2(arr, arrCopy, 0, arr.length-1);
+    	mergeSort2(arr, arrCopy, 0, arr.length);
     }
 
     private static void mergeSort2(int[] arr, int[] aux, int start, int end) {
-    	if((end - start + 1) < 5) {
-    		insertionSort(arr, start, end);
+    	if((end) <= Threshold) {
+            insertionSort(arr, start, start+end-1);
     	}
     	else {
-    		int mid = (end+start)/2;
-    		mergeSort2(arr, aux, start, mid);
-    		mergeSort2(arr, aux, mid+1, end);
-    		merge2(arr, aux, start, mid, end);
+            int mid = (end)/2;
+            mergeSort2(arr, aux, start, mid);
+            mergeSort2(arr, aux, start+mid, end-mid);
+            merge2(arr, aux, start, start+mid-1, start+end-1);
     	}
     }
 
+    // Merge Algorithm for take 2
     private static void merge2(int[] arr, int[] aux, int start, int mid, int end) {
     	System.arraycopy(arr, start, aux, start, end-start+1);
     	int i = start, j = mid+1;
@@ -138,26 +136,26 @@ public class SP9 {
     	}
     }
 
-    //mergesort(Take 3) Implementation
+    //merge sort(Take 3) Implementation
     public static void mergeSort3(int[] arr) {
     	int[] arrCopy = new int[arr.length];
     	System.arraycopy(arr, 0, arrCopy, 0, arr.length);
-    	mergeSort3(arr, arrCopy, 0, arr.length-1);
-    	arr = arrCopy;
+    	mergeSort3(arr, arrCopy, 0, arr.length);
+    	//arr = arrCopy;
     }
 
     private static void mergeSort3(int[] arr, int[] aux, int start, int end) {
-    	if((end - start + 1) < 5) {
-    		insertionSort(arr, start, end);
+    	if(end  <= Threshold) {
+    		insertionSort(arr, start, start+end-1);
     	}
     	else {
-    		int mid = (end+start)/2;
-    		mergeSort3(arr, aux, start, mid);
-    		mergeSort3(arr, aux, mid+1, end);
-    		merge3(aux, arr, start, mid, end);
+    		int mid = (end)/2;
+    		mergeSort3(aux, arr, start, mid);
+    		mergeSort3(aux, arr, mid+start, end-mid);
+    		merge3(arr, aux, start,start+mid-1, start+end-1);
     	}
     }
-
+    // Merge Algorithm for take 3
     private static void merge3(int[] arr, int[] aux, int start, int mid, int end) {
     	int i = start, j = mid+1, k = start;
     	while(i <= mid && j <= end) {
